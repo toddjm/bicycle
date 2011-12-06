df161673751d8104fe3055991ba8e27cd84a3baa
Todd Minehardt <todd@bicycletrading.com>
2011-12-05 18:13:20 -0600

    altered test construct with case.

--- bin/drop_tables.sh
+++ bin/drop_tables.sh
@@ -1,23 +1,31 @@
 #!/bin/bash
+################################################################################
+# Name: drop_tables.sh
+#
+# Drops mysql tables for symbols.
+#
+# Usage: drop_tables.sh <asset class> <symbol>
+# 
 # Copyright 2011 bicycle trading, llc.
+################################################################################
 
 asset_class=$1
 symbol=$2
 
 if [[ $# != 2 ]]
 then
-    echo "Usage: $0 asset_class symbol"
-    exit
+  echo "Usage: $0 asset_class symbol"
+  exit 1
 fi
 
-if [[ $asset_class != "futures" ]] && \
-   [[ $asset_class != "equities" ]] && \
-   [[ $asset_class != "fx" ]]
-then
-    echo "$asset_class does not exist"
-    exit
-fi
-
-for i in 1day 15min 1min 15sec
-    do mysql -e "drop table if exists "$asset_class"_"$i"."$symbol"_tks;"
-done
+case $asset_class in
+  equities | futures | fx)
+  for i in 1day 15min 1min 15sec
+  do mysql -e "drop table if exists "$asset_class"_"$i"."$symbol"_tks;"
+  done
+  ;;
+  *)
+  echo "$asset_class does not exist."
+  exit 1
+  ;;
+esac
