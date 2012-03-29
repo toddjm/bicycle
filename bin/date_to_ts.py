d364b5707f83398bee6aaf8305eba09e9ef8dcf7
Todd Minehardt <todd@bicycletrading.com>
2012-03-29 18:18:37 -0500

    refactored function, set float() upon split().

--- bin/date_to_ts.py
+++ bin/date_to_ts.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
 """
-Convert UTC timestamps to UNIX timestamps and reorganize
-output for tick files to be:
+Convert UTC timestamp strings from mysqldump to
+UNIX timestamps and reorganize output for tick files to be:
 
-UNIX_TIMESTAMP OPEN HIGH LOW CLOSE VOLUME COUNT VWAP GAPS
+TIMESTAMP OPEN HIGH LOW CLOSE VOLUME COUNT VWAP GAPS
+
+where TIMESTAMP, OPEN, HIGH, LOW, CLOSE, and VWAP are
+type float and VOLUME, GAPS, and COUNT are type int.
 
 """
 
@@ -12,14 +15,19 @@ import sys
 import time
 
 __author__ = "Todd Minehardt"
-__copyright__ = "Copyright 2011, bicycle trading, llc"
+__copyright__ = "Copyright 2011, 2012 bicycle trading, llc"
 __email__ = "todd@bicycletrading.com"
 
 
 def date_to_ts(date):
-    """Return UNIX timestamp from UTC timestamp."""
-    return float(calendar.timegm(
-        time.strptime(date, '%Y-%m-%d %H:%M:%S')))
+    """
+    Return UNIX timestamp from UTC timestamp string as
+    returned from a mysqldump.
+
+    """
+    t_struct = time.strptime(date, '%Y-%m-%d %H:%M:%S')
+    t_int = calendar.timegm(t_struct)
+    return float(t_int)
 
 
 def main():
@@ -27,24 +35,24 @@ def main():
     with open(sys.argv[1], 'r') as infile:
         for line in infile:
             timestamp = date_to_ts(line.split()[0] + ' ' + line.split()[1])
-            open_price = line.split()[2]
-            high_price = line.split()[3]
-            low_price = line.split()[4]
-            close_price = line.split()[5]
+            open_price = float(line.split()[2])
+            high_price = float(line.split()[3])
+            low_price = float(line.split()[4])
+            close_price = float(line.split()[5])
             volume = line.split()[6]
-            vwap = line.split()[7]
+            vwap = float(line.split()[7])
             gaps = line.split()[8]
             count = line.split()[9]
-            print '{0} {1} {2} {3} {4} {5} {6} {7} {8}'.format(
-                timestamp,
-                float(open_price),
-                float(high_price),
-                float(low_price),
-                float(close_price),
-                volume,
-                count,
-                float(vwap),
-                gaps)
+            print('{0} {1} {2} {3} {4} {5} {6} {7} {8}').format(
+              timestamp,
+              open_price,
+              high_price,
+              low_price,
+              close_price,
+              volume,
+              count,
+              vwap,
+              gaps)
 
 if __name__ == '__main__':
     main()
