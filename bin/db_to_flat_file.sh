ca795c924459fb0bacc929ac292b5c9f4e547e4a
bicycle <bicycle@bicycletrading.com>
2012-04-27 01:07:42 -0500

    add case clause for proper format for fx.

--- bin/db_to_flat_file.sh
+++ bin/db_to_flat_file.sh
@@ -46,30 +46,39 @@ fi
 #NG201211_tks NG201212_tks NG201105_tks NG201106_tks NG201107_tks NG201108_tks \
 #NG201109_tks NG201110_tks NG201111_tks NG201112_tks
 
-#mysqldump \
-#--no-create-info \
-#--tab=$outdir \
-#--tz-utc \
-#--force \
-#--ignore-table="$asset_class"_15sec.collect \
-#--ignore-table="$asset_class"_15sec.collect_IB_errors \
-#--where="date(ts) >= '2012-01-01' and date(ts) <= '2012-01-05'" \
-#"$asset_class"_15sec AAPL_tks GOOG_tks INTC_tks BAC_tks C_tks
+mysqldump \
+--no-create-info \
+--tab=$outdir \
+--tz-utc \
+--force \
+--ignore-table="$asset_class"_15sec.collect \
+--ignore-table="$asset_class"_15sec.collect_IB_errors \
+--where="date(ts) >= '2012-04-01' and date(ts) <= '2012-04-05'" \
+"$asset_class"_15sec CL201206_tks ZB201209_tks WTI201212_tks
 
 cd $outdir
 
-#for file in `ls *_tks.txt`
-#do
-#  date_to_ts.py $file | sort -nk1 | uniq > $file.ts
-#  rename -f 's/\_tks\.txt\.ts/\.tks/' $file.ts
-#done
-
-for i in `ls *_tks.txt`
-do
-  n=${i%"_tks.txt"}
-  awk -F"\t" 'system("date -ud \""$1"\" +%s")' $i > t
-  awk -F"\t" '{printf "%f %f %f %f %d %d %g %d\n", $2, $3, $4, $5, $6, $9, $7, $8}' $i > d
-  paste -d" " t d > f
-  sort -unk1 f > $n.tks
-  rm -f t d f
-done
+case $asset_class in
+  equities | futures)
+    for i in `ls *_tks.txt`
+    do
+      n=${i%"_tks.txt"}
+      awk -F"\t" 'system("date -ud \""$1"\" +%s")' $i > t
+      awk -F"\t" '{printf "%f %f %f %f %d %d %f %d\n", $2, $3, $4, $5, $6, $9, $7, $8}' $i > d
+      paste -d" " t d > f
+      sort -unk1 f > $n.tks
+      rm -f t d f
+    done
+    ;;
+  fx)
+    for i in `ls *_tks.txt`
+    do
+      n=${i%"_tks.txt"}
+      awk -F"\t" 'system("date -ud \""$1"\" +%s")' $i > t
+      awk -F"\t" '{printf "%f %f %f %f %d %d %d %d\n", $2, $3, $4, $5, $6, $9, $7, $8}' $i > d
+      paste -d" " t d > f
+      sort -unk1 f > $n.tks
+      rm -f t d f
+    done
+    ;;
+esac
