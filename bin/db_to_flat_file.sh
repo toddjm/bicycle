c29b4795bdf2fbc846ca826a333f80e56a3917c0
bicycle <bicycle@bicycletrading.com>
2012-04-28 23:06:44 -0500

    fixed string length calculation.

--- bin/db_to_flat_file.sh
+++ bin/db_to_flat_file.sh
@@ -24,54 +24,38 @@ then
   exit 1
 fi
 
-#mysqldump \
-#--no-create-info \
-#--tab=$outdir \
-#--tz-utc \
-#--force \
-#--ignore-table="$asset_class"_15sec.collect \
-#--ignore-table="$asset_class"_15sec.collect_IB_errors \
-#"$asset_class"_15sec
-
-#mysqldump \
-#--no-create-info \
-#--tab=$outdir \
-#--tz-utc \
-#--force \
-#--ignore-table="$asset_class"_15sec.collect \
-#--ignore-table="$asset_class"_15sec.collect_IB_errors \
+mysqldump \
+--no-create-info \
+--tab=$outdir \
+--tz-utc \
+--force \
+--ignore-table="$asset_class"_15sec.collect \
+--ignore-table="$asset_class"_15sec.collect_IB_errors \
+"$asset_class"_15sec
 #--where="date(ts) >= '2011-02-01' and date(ts) <= '2012-04-09'" \
-#"$asset_class"_15sec NG201201_tks NG201202_tks NG201203_tks NG201204_tks \
-#NG201205_tks NG201206_tks NG201207_tks NG201208_tks NG201209_tks NG201210_tks \
-#NG201211_tks NG201212_tks NG201105_tks NG201106_tks NG201107_tks NG201108_tks \
-#NG201109_tks NG201110_tks NG201111_tks NG201112_tks
-
-#mysqldump \
-#--no-create-info \
-#--tab=$outdir \
-#--tz-utc \
-#--force \
-#--ignore-table="$asset_class"_15sec.collect \
-#--ignore-table="$asset_class"_15sec.collect_IB_errors \
-#--where="date(ts) >= '2012-01-01' and date(ts) <= '2012-04-26'" \
-#"$asset_class"_15sec
 
 cd $outdir
 
-for i in `find -O3 . \! -empty -name "*_tks.txt"`
+rm -f *.sql
+
+find -O3 $outdir -empty -name "*_tks.txt" -exec rm -f '{}' \;
+
+for i in `find -O3 $outdir \! -empty -name "*_tks.txt"`
 do
-  f=${i%"_tks.txt"}.tks
-  date_to_ts.py $i | sort -unk1 > $f
+  file=${i%"_tks.txt"}.tks
+  date_to_ts.py $i | sort -unk1 > $file
 done
 
 if [[ $asset_class == futures ]]
 then
   rm -f *.expiry
-  for i in `find -O3 . \! -empty -name "*.tks"`
+  for i in `find -O3 $outdir \! -empty -name "*.tks"`
     do
-      length=${i#}
+      length=${#i}
       end=$((length - 10))
       file=${i:0:$end}.expiry
       echo ${i:$end:6} >> $file
     done
 fi
+
+find -O3 $outdir -name "*_tks.txt" -exec rm -f '{}' \;
