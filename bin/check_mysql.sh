ac1bb8bb5e0f5c67d9d9de224c587a8a90034a88
bicycle <bicycle@bicycletrading.com>
2012-06-22 12:12:43 -0500

    fixed.

--- bin/check_mysql.sh
+++ bin/check_mysql.sh
@@ -1,41 +1,32 @@
 #!/bin/bash
 
-class=$1
-symbol=$2
+if [[ $# != 1 ]]
+then
+  echo "Usage: `basename $0` <asset class>"
+  exit 1
+fi
 
-case $class in
-  futures)
-  for i in `mysql --skip-column-names -e "select concat(id, contract) from unicycle.futures_roll_dates;"`
-  do
-    echo $i 
-    mysql -N -s -e "select date(t1.ts) from unicycle."$class"_valid_1day \\
-                    as t1 left join "$class"_1day."$i"_tks as t2 on \\
-                    (date(t1.ts) = date(t2.ts)) where date(t2.ts) is null \\
-                    and date(t1.ts) <= (select max(date(ts)) from \\
-                    "$class"_1day."$i"_tks) and date(t1.ts) >= \\
-                    date_sub(now(), interval 6 month);" 2> /dev/null
-    echo
-  done
-#    do echo $i && mysql --skip-column-names -e "select date(t1.ts) \\
-#     from unicycle."$class"_valid_1day as t1 left \\
-#     join "$class"_1day."$i"_tks as t2 on \\
-#     (date(t1.ts) = date(t2.ts)) where date(t2.ts) \\
-#     is null and date(t1.ts) <= (select max(date(ts)) \\
-#     from "$class"_1day."$i"_tks) and \\
-#     date(t1.ts) >= (select min(date(ts)) \\
-#     from "$class"_1day."$i"_tks);"
-#    done
+case $1 in
+equities | futures | fx)
+  asset_class=$1
+  dates=unicycle."$asset_class"_valid_1day
+  db="$asset_class"_1day
+  symbols=unicycle."$asset_class"_symbols
   ;;
-  equities | fx)
-  mysql --skip-column-names -e "select date(t1.ts) \\
-    from unicycle."$class"_valid_1day as t1 left \\
-    join "$class"_1day."$symbol"_tks as t2 on \\
-    (date(t1.ts) = date(t2.ts)) where date(t2.ts) \\
-    is null and date(t1.ts) <= (select max(date(ts)) \\
-    from "$class"_1day."$symbol"_tks) and \\
-    date(t1.ts) >= (select min(date(ts)) from \\
-    "$class"_1day."$symbol"_tks);"
-  ;;
-  *)
+*)
+  echo "Unknown asset class"
+  exit 1
   ;;
 esac
+
+for i in `mysql -N -s -e "select * from "$symbols";"` 
+do
+  echo $i 
+  mysql -N -s -e "select date(t1.ts) from "$dates" \\
+                  as t1 left join "$db"."$i"_tks as t2 on \\
+                  (date(t1.ts) = date(t2.ts)) where date(t2.ts) is null \\
+                  and date(t1.ts) <= (select max(date(ts)) from \\
+                  "$db"."$i"_tks) and date(t1.ts) >= \\
+                  date_sub(now(), interval 6 month);" 2> /dev/null
+  echo
+done
