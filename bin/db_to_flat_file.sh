4b3d45a20715b70ceb7d4bd4b0b649d456edf01a
bicycle <bicycle@bicycletrading.com>
2012-07-04 14:00:03 -0500

    cleaned-up.

--- bin/db_to_flat_file.sh
+++ bin/db_to_flat_file.sh
@@ -11,43 +11,42 @@
 
 if [[ $# < 2 || $# > 3 ]]
 then
-  echo "Usage: `basename $0` asset-class start-date [end-date]"
+  echo "Usage: `basename $0` <asset class> <start date> [end date]"
   exit 1
 fi
 
 class=$1
-s_date=$2
+begin=$2
+
 if [[ $# = 3 ]]
 then
-  e_date=$3
+  end=$3
 else
-  e_date=$(date +%Y-%m-%d)
+  end=$(date +%Y-%m-%d)
 fi
 
-o_dir="$HOME/tmp/$class"
+outdir="$HOME/tmp/$class"
 
-if [[ ! -d $o_dir ]]
+if [[ ! -d $outdir ]]
 then
-  echo "$o_dir not found."
+  echo "$outdir not found."
   exit 1
 fi
 
-cd $o_dir
-rm -f $o_dir/*.*
+cd $outdir
+rm -f $outdir/*.*
 
 mysqldump \
 --no-create-info \
---tab=$o_dir \
+--tab=$outdir \
 --tz-utc \
 --force \
 --ignore-table="$class"_15sec.collect \
 --ignore-table="$class"_15sec.collect_IB_errors \
---where="date(ts) >= '"$s_date"' and date(ts) <= '"$e_date"'" \
+--where="date(ts) >= '"$begin"' and date(ts) <= '"$end"'" \
 "$class"_15sec
-#--where="date(ts) >= '2012-01-01' and date(ts) <= '2012-06-15'" \
-#"$class"_15sec
 
-list=$(find -O3 $o_dir \! -empty -name "*_tks.txt")
+list=$(find -O3 $outdir \! -empty -name "*_tks.txt")
 for i in $list
 do
   file=${i%"_tks.txt"}.tks
@@ -56,7 +55,7 @@ done
 
 if [[ $class = futures ]]
 then
-  list=$(find -O3 $o_dir \! -empty -name "*.tks")
+  list=$(find -O3 $outdir \! -empty -name "*.tks")
   for i in $list
     do
       length=${#i}
