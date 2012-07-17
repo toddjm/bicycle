a23443b862fa54250c056c9e2d456361f8072dab
bicycle <bicycle@bicycletrading.com>
2012-07-17 01:08:39 -0500

    cleaned spacing.

--- bin/make_subdirs_by_date.sh
+++ bin/make_subdirs_by_date.sh
@@ -10,49 +10,49 @@ years="2009 2010 2011 2012 2013 2014 2015 2016 2017 2018 2019 2020 2021 2022"
 # correct usage
 if [[ ! $# == 2 ]]
 then
-    echo "Usage: $0 exchange symbol"
-    exit
+  echo "Usage: `basename $0` exchange symbol"
+  exit 1
 fi
 
 # make sure we are in an exchange directory
 if [[ `basename $PWD` != $exchange ]]
 then
-    echo "Not in correct directory. Exiting..."
-    exit
+  echo "Not in correct directory. Exiting."
+  exit 2
 fi
 
 # make sure the source directory exists
 if [[ ! -d $srcdir ]]
 then
-    echo "$srcdir not found. Exiting..."
-    exit
+  echo "$srcdir not found. Exiting."
+  exit 3
 fi
 
 # make directories if needed
 for i in $years
 do
-    if [[ $i == 2009 ]]
-    then
-        for j in 09 10 11 12
-        do
-            destination="./$symbol/$i/$j"
-            if [[ ! -d $destination ]]
-            then
-                echo "Creating $destination"
-                mkdir -p $destination
-            fi
-        done
-    else
-        for j in $months
-        do
-            destination="./$symbol/$i/$j"
-            if [[ ! -d $destination ]]
-            then
-                echo "Creating $destination"
-                mkdir -p $destination
-            fi
-        done
-    fi
+  if [[ $i == 2009 ]]
+  then
+    for j in 09 10 11 12
+    do
+      destination="./$symbol/$i/$j"
+      if [[ ! -d $destination ]]
+      then
+        echo "Creating $destination"
+        mkdir -p $destination
+       fi
+    done
+  else
+    for j in $months
+    do
+      destination="./$symbol/$i/$j"
+      if [[ ! -d $destination ]]
+      then
+        echo "Creating $destination"
+        mkdir -p $destination
+      fi
+    done
+  fi
 done
 
 # copy files to destination directories
