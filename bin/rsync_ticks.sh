211f2527d11b459ebd50790f0e994f1cb7f9885f
bicycle <bicycle@bicycletrading.com>
2012-07-17 12:51:53 -0500

    added case for cmd line args.

--- bin/rsync_ticks.sh
+++ bin/rsync_ticks.sh
@@ -1,13 +1,39 @@
 #!/bin/bash
-############################################
+#####################################################################
 #
 # rsync script, run as bicycle
 #
+# usage:
+# rsync_ticks.sh [<asset class> <asset class> <asset class>]
+#
 # Copyright 2011, 2012 bicycle trading, llc
 #
-############################################
+#####################################################################
 
-classes="equities futures fx"
+if [[ $# > 3 ]]
+then
+  echo "Usage: `basename $0` [<asset class> <asset class> <asset class>]"
+  exit 1
+fi
+
+if [[ $# == 0 ]]
+then
+  classes="fx"
+else
+  for i in $@
+  do
+    case $i in
+      equities | futures | fx)
+        classes="$classes $i"
+      ;;
+      *)
+        echo "$i not found. Exiting."
+        exit 1
+      ;;
+    esac
+  done
+fi
+
 dir=$TICKS_HOME
 hosts="x0 x1 x2 x3 y0 y1 y2"
 options="-aPuvz --delete --ignore-existing"
@@ -19,3 +45,5 @@ do
     rsync $options $dir/$i bicycle@$j:$dir
   done
 done
+
+echo $?
