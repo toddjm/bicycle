ae26232ae653f573aac3adc875b11547fb00bf19
bicycle <bicycle@bicycletrading.com>
2012-07-25 15:54:56 -0500

    refactor and clean.

--- bin/create_ticker_plant.py
+++ bin/create_ticker_plant.py
@@ -53,30 +53,24 @@ def create_tks_files(config, options):
     end = options.parse_args().end
     start = datetime.datetime.strptime(start, '%Y-%m-%d %H:%M:%S')
     end = datetime.datetime.strptime(end, '%Y-%m-%d %H:%M:%S')
-    #
-    root = config[group]['root']
-    srcdir = os.path.join(root, source)  # like /home/bicycle/tmp/futures/ib
-    symbols = get_symbols(srcdir)  # from the symbols.txt file
-
-
+    srcdir = config[group]['root']  # like /home/bicycle/tmp/futures
+    exchanges = set_exchanges(config, group, source)
     if group == 'futures':
+        symbols = set_symbols(config, group, source)
         for exchange in exchanges:
-            expiry = get_expiry(srcdir, symbols)
-            for symbol in expiry.keys():
+            expiry = get_expiry(srcdir, symbols[exchange])
+            for symbol in expiry.keys():  # filters-out unlisted symbols
                 for contract in expiry[symbol]:
                     data = read_tks_file(srcdir, symbol, contract=contract)
-                    path = os.path.join(os.getenv('TICKS_HOME'),
-                                        group,
-                                        source,
-                                        exchange,
-                                        symbol,
-                                        contract)
+                    path = os.path.join(os.getenv('TICKS_HOME'), group,
+                                        source, exchange, symbol, contract)
                     print("Writing ticks for {0}{1}...").format(symbol,
                                                                 contract)
                     write_tks_file(start, end, symbol, data, path)
     else:
+        symbols = get_symbols(srcdir)
         for exchange in exchanges:
-            for symbol in symbols[exchange]:
+            for symbol in symbols:
                 data = read_tks_file(srcdir, symbol)
                 path = os.path.join(os.getenv('TICKS_HOME'), group, source,
                                     exchange, symbol)
@@ -101,12 +95,13 @@ def find_le(values, threshold):
     raise ValueError
 
 
-def get_expiry(source_dir, symbols):
+def get_expiry(srcdir, symbols):
     """Returns a dict of contract expirations keyed on symbol."""
     values = {}
-    for i in os.listdir(source_dir):
-        if i.endswith('.expiry'):
-            values[symbol] = read_file(source_dir, symbol + '.expiry')
+    for i in symbols:
+        infile = os.path.join(srcdir, i + '.expiry')
+        if os.path.isfile(infile) and os.path.getsize(infile):
+            values[i] = read_file(srcdir, i + '.expiry')
     return values
 
 
@@ -131,11 +126,12 @@ def get_subset(values, threshold):
     return subset
 
 
-def get_symbols(source_dir):
-    """Returns list of symbols."""
+def get_symbols(srcdir):
+    """Get symbols for which tks files are non-zero length."""
     values = []
-    if os.path.isfile(os.path.join(source_dir, 'symbols.txt')):
-        values = read_file(source_dir, 'symbols.txt')
+    for i in os.listdir(srcdir):
+        if i.endswith('.tks'):
+            values.append(i.strip('.tks'))
     return values
 
 
@@ -155,32 +151,37 @@ def read_file(path, name):
     return values
 
 
-#def get_exchanges_symbols(config, group, source):
-#    """Returns exchanges list and symbols dict as pair of tuples."""
-#    exchanges = [i for i in config[group][source].iterkeys()]
-#    symbols = {}
-#    if group == 'equities':
-#        symbol_file = {}
-#        for i in config[group][source].iterkeys():
-#            symbol_file[i] = config[group][source][i]
-#            with open(symbol_file[i], 'r') as infile:
-#                symbol_list = infile.readlines()
-#            symbols[i] = [i.strip() for i in symbol_list]
-#    else:
-#        for i in config[group][source].iterkeys():
-#            symbols[i] = config[group][source][i]
-#    return exchanges, symbols
+def get_exchanges_symbols(config, group, source):
+    """Returns exchanges list and symbols dict as tuple."""
+    exchanges = [i for i in config[group][source].iterkeys()]
+    symbols = {}
+    if group == 'equities':
+        symbol_file = {}
+        for i in config[group][source].iterkeys():
+            symbol_file[i] = config[group][source][i]
+            with open(symbol_file[i], 'r') as infile:
+                symbol_list = infile.readlines()
+            symbols[i] = [i.strip() for i in symbol_list]
+    else:
+        for i in config[group][source].iterkeys():
+            symbols[i] = config[group][source][i]
+    return exchanges, symbols
 
 
-
-
-def read_tks_file(source_dir, symbol, **kwargs):
+def read_tks_file(srcdir, symbol, **kwargs):
     """Read tks file, return list sorted on timestamp. """
     contract = kwargs.get('contract', "")
-    infile = os.path.join(source_dir, symbol + contract + '.tks')
+    infile = os.path.join(srcdir, symbol + contract + '.tks')
     if os.path.isfile(infile) and os.path.getsize(infile):
-        values = read_file(source_dir, symbol + contract + '.tks')
+        values = read_file(srcdir, symbol + contract + '.tks')
     return sorted(values, key=lambda x: x[0])
+
+
+def set_exchanges(config, group, source):
+    """Return list of exchanges."""
+    values = []
+    values = [i for i in config[group][source].iterkeys()]
+    return values
 
 
 def set_parser():
@@ -230,6 +231,22 @@ def set_start_end(start, end, data):
     return start, end
 
 
+def set_symbols(config, group, source):
+    """Returns dict of symbols keyed on exchanges."""
+    values = {}
+    if group == 'equities':
+        symbol_file = {}
+        for i in config[group][source].iterkeys():
+            symbol_file[i] = config[group][source][i]
+            with open(symbol_file[i], 'r') as infile:
+                symbol_list = infile.readlines()
+            values[i] = [i.strip() for i in symbol_list]
+    else:
+        for i in config[group][source].iterkeys():
+            values[i] = config[group][source][i]
+    return values
+
+
 def write_tks_file(start, end, symbol, data, path):
     """Write ticks to files with .tks suffix."""
     # Adjust beginning ('now') and end ('end') if needed.
@@ -239,22 +256,19 @@ def write_tks_file(start, end, symbol, data, path):
         if check_date(now):
             # Extract subset of data for this day only.
             subset = get_subset(data, now)
-            # Make sure subset length is non-zero.
             if subset:
-                # Set directory for writing ticks.
+                # Set directory for writing ticks, create if needed.
                 outdir = os.path.join(path,
                                       '{0:04d}'.format(now.year),
                                       '{0:02d}'.format(now.month),
                                       '{0:02d}'.format(now.day))
-                # Create directory if required.
                 if not os.path.isdir(outdir):
                     os.makedirs(outdir, 0755)
                 # Set tks file for output.
                 tks = os.path.join(outdir, symbol + '.tks')
-                # If tks file exists and is zero bytes, remove it.
+                # If tks file exists and is zero bytes, remove and re-create.
                 if os.path.isfile(tks) and not os.path.getsize(tks):
                     os.remove(tks)
-                # Create tks file.
                 with open(tks, 'w') as outfile:
                     for i in subset:
                         outfile.write(i + '\n')
@@ -269,11 +283,11 @@ def main():
 
     """
     # Read configuration file, parse command line arguments.
-    cfg = configobj.ConfigObj(os.path.join(os.getenv('BICYCLE_HOME'),
-                                           'config.ini'))
+    config = configobj.ConfigObj(os.path.join(os.getenv('BICYCLE_HOME'),
+                                              'config.ini'))
     parser = set_parser()
 
-    create_tks_files(cfg, parser)
+    create_tks_files(config, parser)
 
 
 if __name__ == '__main__':
